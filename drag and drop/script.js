a16d9f92b84e9ecbd09748a1de37cf8c2e88c570
vikram-sharma-96 <vikram.25b01010075@abes.ac.in>
2026-07-28 21:51:45 +0530

    working on drag and drop object

---
+++ drag and drop/script.js
@@ -0,0 +1,9 @@
+let lists = document.getElementById("list");
+let rightBox = document.getElementById("right");
+let leftBox = document.getElementById("left");
+
+for(list of lists){
+    list.addEventListener("dragstart",function(e){
+        let selected = e.target;
+    })
+}
